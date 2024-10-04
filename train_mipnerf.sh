cd6cb485d501953db2e6640d0a160a5483e6ff4a
Edvards Zakovskis <edvards.zakovskis@giraff360.com>
2024-10-04 15:29:10 +0300

    building the viewer

--- train_mipnerf.sh
+++ train_mipnerf.sh
@@ -1,14 +1,16 @@
 
 runtype="runs"
-scenes="bicycle bonsai counter garden room stump kitchen"
+scenes="bicycle"
 
+echo "DATASET"
+echo $DATASET_DIR/bicycle
 for scene in $scenes; do  
 
-    outdir="./output/${runtype}/mipnerf/${scene}"
+    outdir="./output/${scene}"
     mkdir -p $outdir
 
     python train.py \
-     -s /path/to/mipnerf360_dataset/${scene} --eval -r 4 \
-     -m $outdir --port $port --data_device cuda \
+     -s $DATASET_DIR/$scene --eval -r 4 \
+     -m $outdir --port 8000 --data_device cuda \
      
 done
